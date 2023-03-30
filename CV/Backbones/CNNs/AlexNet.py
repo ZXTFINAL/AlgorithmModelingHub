a515377aff7ac3d11e52536469442a1d03532ec9
ZXTFINAL <53009474+ZXTFINAL@users.noreply.github.com>
2023-03-30 16:06:16 +0800

    <feat>add backbones

--- CV/Backbones/CNNs/AlexNet.py
+++ CV/Backbones/CNNs/AlexNet.py
@@ -6,36 +6,37 @@ from torchsummary import summary
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-class LeNet5(nn.Module):
+class AlexNet5(nn.Module):
     def __init__(self, n_classes):
         super(LeNet5, self).__init__()
         self.Conv_1 = nn.Conv2d(
-            in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=0)
-        self.Sigmoid_1 = nn.Sigmoid()
+            in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=0)
+        self.Relu_1 = nn.ReLU()
         self.pooling_1 = nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
         self.Conv_2 = nn.Conv2d(
             in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0)
-        self.Sigmoid_2 = nn.Sigmoid()
+        self.Relu_2 = nn.ReLU()
         self.pooling_2 = nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
-        self.Fc_1 = nn.Linear(16*5*5, 120)
-        self.Sigmoid_3 = nn.Sigmoid()
-        self.Fc_2 = nn.Linear(120, 84)
-        self.Sigmoid_4 = nn.Sigmoid()
-        self.Fc_3 = nn.Linear(84, n_classes)
+        self.Conv_3 = nn.Conv2d(
+            in_channels=16, out_channels=120, kernel_size=5, stride=1, padding=0)
+        self.Relu_3 = nn.ReLU()
+        self.Fc_1 = nn.Linear(120, 84)
+        self.Relu_4 = nn.ReLU()
+        self.Fc_2 = nn.Linear(84, n_classes)
 
     def forward(self, x):
         x = self.Conv_1(x)
-        x = self.Sigmoid_1(x)
+        x = self.Relu_1(x)
         x = self.pooling_1(x)
         x = self.Conv_2(x)
-        x = self.Sigmoid_2(x)
+        x = self.Relu_2(x)
         x = self.pooling_2(x)
+        x = self.Conv_3(x)
+        x = self.Relu_3(x)
         x = x.view(x.size(0), -1)
         x = self.Fc_1(x)
-        x = self.Sigmoid_3(x)
+        x = self.Relu_4(x)
         x = self.Fc_2(x)
-        x = self.Sigmoid_4(x)
-        x = self.Fc_3(x)
         return x
 
 
